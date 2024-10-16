ac58b69070cc07fd8438b6536bda8b2f8850dbc7
oakwoodgroupie <ivanfrantar@fastmail.com>
2024-10-16 09:41:18 +0100

    Added a javascript preloading to the pages.It seems to have solved the issue.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,30 @@
-// YOU CAN USE THE VERSION I HAVE ON MY WEBSITE.
- 
-//document.addEventListener('scroll', () => {
-//     const nav = document.querySelector('nav');
-//     const scrollPosition = window.scrollY;
+// Preloading fonts
+const fonts = [
+    new FontFace('Satoshi', 'url(/fonts/Satoshi-Variable.woff2)', {
+      weight: '200 900',
+      style: 'normal',
+    }),
+    new FontFace('Satoshi Italic', 'url(/fonts/Satoshi-VariableItalic.woff2)', {
+      weight: '200 900',
+      style: 'italic',
+    }),
+    new FontFace('Bitter', 'url(/fonts/Bitter-VariableFont_wght.woff2)', {
+      weight: '200 900',
+      style: 'normal',
+    }),
+    new FontFace('Bitter Italic', 'url(/fonts/Bitter-Italic-VariableFont_wght.woff2)', {
+      weight: '200 900',
+      style: 'italic',
+    })
+  ];
   
-//     if (scrollPosition > 80) {
-//       nav.classList.add('sticky-header');
-//     } else if (scrollPosition < 500) {
-//       nav.classList.remove('sticky-header');
-//     }
-// });
+  // Load the fonts
+  fonts.forEach(font => {
+    font.load().then((loadedFont) => {
+      document.fonts.add(loadedFont);
+      console.log(`${loadedFont.family} loaded successfully`);
+    }).catch((error) => {
+      console.error(`Failed to load font: ${error}`);
+    });
+  });
+  
